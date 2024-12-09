3e39121704449d89c963115c2fa59342ec23102a
Porom <poromarte@gmail.com>
2024-12-09 19:11:41 +0100

    .commit primero

---
+++ src/main/java/org/example/ejerciciodificilspring/EjercicioDificilSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.ejerciciodificilspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioDificilSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjercicioDificilSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ejerciciodificilspring/EjercicioDificilSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ejerciciodificilspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioDificilSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
